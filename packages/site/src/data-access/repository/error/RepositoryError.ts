169ffc17038a202424b2f018093d114ced33d8c6
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-06-17 09:28:51 +0200

    refactor(site): improve error handling and improve ui

--- packages/site/src/data-access/repository/error/RepositoryError.ts
+++ packages/site/src/data-access/repository/error/RepositoryError.ts
@@ -1,12 +1,17 @@
-import type { RepositoryErrorCode } from './RepositoryErrorCodes';
+import { AnyObject } from '@swisstype/essential';
+
+import { RepositoryErrorCode } from './RepositoryErrorCodes';
 
 export default class RepositoryError extends Error {
   code: RepositoryErrorCode;
 
-  constructor(code: RepositoryErrorCode) {
+  data?: AnyObject;
+
+  constructor(code: RepositoryErrorCode, data?: AnyObject) {
     super(code);
 
     this.name = 'RepositoryError';
     this.code = code;
+    this.data = data;
   }
 }
