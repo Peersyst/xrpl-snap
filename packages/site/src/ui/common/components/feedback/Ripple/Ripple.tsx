267aeb084691294a619afb5424ad03a9f24106a8
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-08-30 13:28:14 +0200

    feat(site): only show the app in desktop

---
+++ packages/site/src/ui/common/components/feedback/Ripple/Ripple.tsx
@@ -0,0 +1,48 @@
+/* s/o to Magic UI https://magicui.design/docs/components/ripple */
+import React from 'react';
+
+import { RippleCircle, RippleRoot } from './Ripple.styles';
+
+export interface RippleConfig {
+  mainCircleSize: number;
+  mainCircleOpacity: number;
+  numCircles: number;
+  top: string;
+}
+
+const RIPPLE_CONFIG = {
+  desktop: {
+    mainCircleSize: 595,
+    mainCircleOpacity: 0.24,
+    numCircles: 6,
+    top: '20%',
+  },
+  mobile: {
+    mainCircleSize: 200,
+    mainCircleOpacity: 0.24,
+    numCircles: 6,
+    top: '46.5%',
+  },
+};
+
+export interface RippleProps {
+  config?: RippleConfig;
+  mobile?: boolean;
+}
+
+const Ripple = React.memo(function Ripple({ config, mobile = false }: RippleProps) {
+  const { mainCircleSize, mainCircleOpacity, numCircles, top } = config || (mobile ? RIPPLE_CONFIG.mobile : RIPPLE_CONFIG.desktop);
+  return (
+    <RippleRoot className="Ripple" mobile={mobile}>
+      {Array.from({ length: numCircles }, (_, i) => {
+        const size = mainCircleSize + i * 150; // px
+        const opacity = mainCircleOpacity - i * 0.03;
+        const animationDelay = `${i * 0.06}s`;
+
+        return <RippleCircle className="RippleCircle" key={i} top={top} size={size} opacity={opacity} delay={animationDelay} />;
+      })}
+    </RippleRoot>
+  );
+});
+
+export default Ripple;
---
+++ packages/site/src/ui/common/pages/DesktopOnlyPage/DesktopOnlyPage.tsx
@@ -0,0 +1,34 @@
+import { Col, Image, Typography } from '@peersyst/react-components';
+import clsx from 'clsx';
+import { nomobile } from 'ui/assets/images';
+import Ripple from 'ui/common/components/feedback/Ripple/Ripple';
+import { useTranslate } from 'ui/locale';
+
+import { DesktopOnlyPageRoot } from './DesktopOnlyPage.styles';
+
+export interface DesktopOnlyPageProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+function DesktopOnlyPage({ className, ...rest }: DesktopOnlyPageProps) {
+  const translate = useTranslate();
+  return (
+    <DesktopOnlyPageRoot className={clsx('DesktopOnlyPage', className)} {...rest}>
+      <Ripple mobile />
+      <Col alignItems="center">
+        <Image className="SnapLogoNoMobile" src={nomobile} alt="no-mobile" />
+        <Col css={{ maxWidth: '70vw', marginTop: '-2rem' }} gap="0.5rem">
+          <Typography variant="h3" textAlign="center">
+            {translate('snapOnlyAvailableInDesktop')}
+          </Typography>
+          <Typography variant="body1" textAlign="center">
+            {translate('snapOnlyAvailableInDesktopText')}
+          </Typography>
+        </Col>
+      </Col>
+    </DesktopOnlyPageRoot>
+  );
+}
+
+export default DesktopOnlyPage;
